dd2cab3d84b2ce47ccf2c48b5890b506e493c9ac
Drilon <navycrack1@gmail.com>
2022-02-13 19:53:58 +0100

    finishing navbar responsive

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,6 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
+import lartana from '../assets/lartana.PNG'
 import G from '../assets/G.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
@@ -19,7 +20,6 @@ import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
 
 export default {
   bg,
@@ -43,5 +43,5 @@ export default {
   award05,
   sign,
   quote,
-  gericht,
+  lartana,
 };
